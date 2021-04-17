14cf3eaf2ccae368ba62728545e84ef91f654c30
2024acheong <82597699+2024acheong@users.noreply.github.com>
2021-04-16 20:26:27 -0400

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -6,7 +6,7 @@ public abstract class Sprite {
     double x;
     double y;
     int direction;
-    protected Sprite(double a, double b, int d, BufferedImage i, JPanel p) {
+    protected Sprite(double a, double b, int d, BufferedImage i, JPanel p) { //This constructor will draw an image on to the inputted JPanel. a is the x coordinate, b is the y coordinate.
         x = a;
         y = b;
         direction = d;
