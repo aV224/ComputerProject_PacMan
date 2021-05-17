2c4d99b38ac4cdf49887c94001ccd35b1ab175a0
2024acheong <82597699+2024acheong@users.noreply.github.com>
2021-05-17 14:48:10 -0400

    Update Pellet.java

--- Pellet.java
+++ Pellet.java
@@ -1,14 +1,17 @@
 
 import java.awt.image.BufferedImage;
 public class Pellet extends Sprite{
-    private boolean eaten;
+    private int eaten;
 
     public Pellet(int x, int y, BufferedImage img) {
         super(x, y, img);
     }
 
     public boolean isEaten(Sprite b) {
-        eaten = this.checkCollision(b);
+        eaten = 0;
+        if (this.checkCollision(b)) {
+            eaten = 1;
+        }
         return eaten;
     }
 }
