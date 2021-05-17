9f6e61c6044c3b218b895b9567541985d987c456
2024acheong <82597699+2024acheong@users.noreply.github.com>
2021-05-17 15:23:13 -0400

    Update Powerup.java

--- Powerup.java
+++ Powerup.java
@@ -1,14 +1,16 @@
 
 import java.awt.image.BufferedImage;
 public class Powerup extends Sprite{
-    private boolean eaten;
+    protected int eaten = 0;
 
     public Powerup(int x, int y, BufferedImage img) {
         super(x, y, img);
     }
 
-    public boolean isEaten(Sprite b) {
-        eaten = this.checkCollision(b);
+    public int isEaten(Sprite b) {
+        if (this.checkCollision(b)) {
+            eaten += 1;
+        }
         return eaten;
     }
 }
