95616a8b94376bee1da75291a89c8c7ef98c7969
2024acheong <82597699+2024acheong@users.noreply.github.com>
2021-05-20 20:52:18 -0400

    Update Sprite.java

--- PacManUpload20520/Sprite.java
+++ PacManUpload20520/Sprite.java
@@ -1,44 +1,79 @@
+/**
+*This is the abstract sprite class for all the objects that get painted on screen
+*@author Aarya Vijayaraghavan
+*@author Aiden Cheong
+*/
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public abstract class Sprite {
-
+	/**
+	*Stores the x-coordinate of the sprite
+	*/
 	protected volatile int xAxis = 0;
+	/**
+	*Stores the y-coordinate of the sprite
+	*/
 	protected volatile int yAxis = 0;
+	/**
+	*Stores the width of the sprite
+	*/
 	protected volatile int width = 0;
+	/**
+	*Stores the height of the sprite
+	*/
 	protected volatile int height = 0;
+	/**
+	*Stores the sprite image
+	*/
 	protected volatile BufferedImage img = null;
 
 	protected static final int moduloValue = 20;
-
+	/**
+	*@return width
+	*/
 	public int getWidth() {
 		return width;
 	}
-
+	/**
+	*Sets the width of the sprite
+	*/
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
+	/**
+	*@return height
+	*/
 	public int getHeight() {
 		return height;
 	}
-
+	/**
+	*Sets the height of the sprite
+	*/
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+	/**
+	*@return xAxis
+	*/
 	public int getxAxis() {
 		return xAxis;
 	}
-
+	/**
+	*Sets the x-coordinate of the sprite
+	*/
 	public void setxAxis(int xAxis) {
 		this.xAxis = xAxis;
 	}
-
+	/**
+	*@return yAxis
+	*/
 	public int getyAxis() {
 		return yAxis;
 	}
-
+	/**
+	*sets the y-coordinate of the sprite
+	*/
 	public void setyAxis(int yAxis) {
 		this.yAxis = yAxis;
 	}
