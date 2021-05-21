f0ecc477a079b915f6e7cc81ed0e9f81323be5bf
2024acheong <82597699+2024acheong@users.noreply.github.com>
2021-05-20 20:54:16 -0400

    Update Pellet.java

--- Pellet.java
+++ Pellet.java
@@ -1,4 +1,7 @@
-
+/**
+*This is the pellet class (also known as the fruit class)
+*@author Aiden Cheong
+*/
 import java.awt.image.BufferedImage;
 public class Pellet extends Sprite{
     protected int eaten = 0;
@@ -6,7 +9,10 @@ public class Pellet extends Sprite{
     public Pellet(int x, int y, BufferedImage img) {
         super(x, y, img);
     }
-
+    /**
+    *If the pellet is eaten, 1 is added to eaten
+    *@return eaten
+    */
     public int isEaten(Sprite b) {
         if (this.checkCollision(b)) {
             eaten += 1;
