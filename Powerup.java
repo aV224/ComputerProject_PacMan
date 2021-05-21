93bdbaa7c4283b8c209a5e959f05be03ed4bfa1b
2024acheong <82597699+2024acheong@users.noreply.github.com>
2021-05-20 20:55:36 -0400

    Update Powerup.java

--- Powerup.java
+++ Powerup.java
@@ -1,4 +1,7 @@
-
+/**
+* This is the powerup class. When eaten, this will turn the ghosts into a frightened state
+*@author Aiden Cheong
+*/
 import java.awt.image.BufferedImage;
 public class Powerup extends Sprite{
     protected int eaten = 0;
@@ -6,7 +9,10 @@ public class Powerup extends Sprite{
     public Powerup(int x, int y, BufferedImage img) {
         super(x, y, img);
     }
-
+    /**
+    *If the powerup is eaten, 1 is added to eaten
+    *@return eaten
+    */
     public int isEaten(Sprite b) {
         if (this.checkCollision(b)) {
             eaten += 1;
